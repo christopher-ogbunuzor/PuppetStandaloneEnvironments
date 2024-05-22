ea22cd8f7462bac45549cbaf696c94a9afa2d333
Christopher Ogbunuzor <ogbunuzorchristopher@yahoo.com>
2024-05-22 16:43:28 +0100

    add os hardening - sorting sysctl

--- sortoutmodules/modules/my_variables/manifests/local_variables.pp
+++ sortoutmodules/modules/my_variables/manifests/local_variables.pp
@@ -18,4 +18,9 @@ class my_variables::local_variables {
     content => "Another ${my_content}",
     path    => '/tmp/local_variables.txt',
   }
+
+  sysctl { "net.ipv4.ip_forward":
+    ensure => present,
+    value  => "1",
+  }
 }
